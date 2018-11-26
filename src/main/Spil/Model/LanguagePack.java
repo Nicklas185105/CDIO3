426ea26405120fa88c40c1c6beb16b8f46da02c9
Rasmus <rsoeborg@gmail.com>
2018-11-26 10:09:34 +0100

    Ændret gameStrings direkte til et HashMap fra Dictionary interfacet

--- src/main/Spil/Model/LanguagePack.java
+++ src/main/Spil/Model/LanguagePack.java
@@ -21,7 +21,7 @@ public class LanguagePack {
     /**
      *
      */
-    Dictionary<String, String> gameStrings;
+    HashMap<String, String> gameStrings;
 
     /**
      * This constructor is used for calling the method parseFile
