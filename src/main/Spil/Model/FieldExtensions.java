6180f2c0a70f78e447d5a6ca4eaa1e554c543084
Rasmus <rsoeborg@gmail.com>
2018-11-27 17:01:36 +0100

    Lavet FieldExtensions class til at tjekke om fields er ownable og/eller konverterer et field til et GUI_Ownable på en sikker måde.

--- src/main/Spil/Model/FieldExtensions.java
+++ src/main/Spil/Model/FieldExtensions.java
@@ -1,4 +1,23 @@
 package main.Spil.Model;
 
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Ownable;
+
 public class FieldExtensions {
+    public static boolean isOwnableField(Field field) {
+        return isOwnableField(field.toGUI());
+    }
+
+    public static boolean isOwnableField(GUI_Field guiField) {
+        return guiField instanceof GUI_Ownable;
+    }
+
+    public GUI_Ownable toOwnable(Field field) {
+        return toOwnable(field.toGUI());
+    }
+
+    public GUI_Ownable toOwnable(GUI_Field guiField) {
+        if (isOwnableField(guiField)) { return (GUI_Ownable)guiField; }
+        throw new IllegalArgumentException("Field is not of type GUI_Ownable");
+    }
 }
