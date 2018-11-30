ffdfc1971b4ab5c93394f89c8861838a40757b79
Rasmus <rsoeborg@gmail.com>
2018-11-30 16:13:07 +0100

    Sat et try-catch op

--- src/main/Spil/Model/PropertyFieldActionListener.java
+++ src/main/Spil/Model/PropertyFieldActionListener.java
@@ -4,37 +4,41 @@ import gui_fields.GUI_Ownable;
 
 public class PropertyFieldActionListener implements FieldActionListener {
     public void onFieldLandedOn(FieldAction action) {
-        if (Field.isOwnableField(action.field)) {
-            // Retrieve field
-            GUI_Ownable ownableField = Field.toOwnable(action.guiField);
+        try {
+            if (Field.isOwnableField(action.field)) {
+                // Retrieve field
+                GUI_Ownable ownableField = Field.toOwnable(action.guiField);
 
-            int rent = Integer.parseInt(
-                    ownableField.getRent()
-                            .split(":")[1]
-                            .replace(" ", "")
-            );
+                int rent = Integer.parseInt(
+                        ownableField.getRent()
+                                .split(":")[1]
+                                .replace(" ", "")
+                );
 
-            // Check if the field is for sale?
-            if (ownableField.getOwnerName() == null && action.player.getBalance() >= rent) {
-                // Prompt dialog to buy property?
-                if (action.gameState.getView().getUserSelection(String.format("Denne bolig er til salg! Vil %s købe denne bolig?", action.player.getName()), "yes", "no").equals("yes")) {
-                    ownableField.setOwnerName(action.player.getName());
+                // Check if the field is for sale?
+                if (ownableField.getOwnerName() == null && action.player.getBalance() >= rent) {
+                    // Prompt dialog to buy property?
+                    if (action.gameState.getView().getUserSelection(String.format("Denne bolig er til salg! Vil %s købe denne bolig?", action.player.getName()), "yes", "no").equals("yes")) {
+                        ownableField.setOwnerName(action.player.getName());
+                        action.player.setBalance(action.player.getBalance() - rent);
+                    }
+                }
+
+                // Require rent from other players
+                else if (!ownableField.getOwnerName().equals(action.player.getName())) {
+                    action.gameState.getView().showMessage(String.format("%s er landet på et felt som er ejet af %s. Du skulle betale %s til ham!",
+                            action.player.getName(),
+                            ownableField.getOwnerName(),
+                            ownableField.getRent()));
+
+                    // Remove money from player pocket
                     action.player.setBalance(action.player.getBalance() - rent);
+                    Player owner = findPlayer(action.gameState.getPlayers(), ownableField.getOwnerName());
+                    owner.setBalance(owner.getBalance() + rent);
                 }
             }
+        } catch (NullPointerException npe) {
 
-            // Require rent from other players
-            else if (!ownableField.getOwnerName().equals(action.player.getName())) {
-                action.gameState.getView().showMessage(String.format("%s er landet på et felt som er ejet af %s. Du skulle betale %s til ham!",
-                        action.player.getName(),
-                        ownableField.getOwnerName(),
-                        ownableField.getRent()));
-
-                // Remove money from player pocket
-                action.player.setBalance(action.player.getBalance() - rent);
-                Player owner = findPlayer(action.gameState.getPlayers(), ownableField.getOwnerName());
-                owner.setBalance(owner.getBalance() + rent);
-            }
         }
     }
 
